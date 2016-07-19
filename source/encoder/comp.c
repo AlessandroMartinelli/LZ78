a93df4fdb6a03ed999e64c1be7172cf37d16cf49
Francesco <fpculcasi@gmail.com>
2016-07-19 12:21:09 +0200

    comp
    - Generated the header of the compr file
    - While loop is optimized
    - bitio_write() pull character and parent_id separately, to exploit the variable length of the parent_id
    
    TODO: what's the size of the parent_id? (see "XXX" tag)

--- source/encoder/comp.c
+++ source/encoder/comp.c
@@ -1,69 +1,90 @@
 #include "comp.h"
 
-int comp(const char *filename_dec, const char *filename_enc){
-	struct bitio *b_enc, *b_dec;
+int comp(const char *filename_in, const char *filename_out, uint32_t dictionary_size){
+	struct bitio *b_out, *b_in;
 	uint16_t next_id = 1;
 	uint16_t parent_id = 0;
-	uint64_t result;
-	char aux;
+	int res;
+	char aux_char;
+	uint32_t aux_32;
+	uint64_t aux_64;
 	list_t *node;
 	
-	printf("comp\n");
-	
-	if(check_ext(filename_enc, "lz78") || check_ext(filename_dec, "txt")){
+	/*
+	if(check_ext(filename_out, "lz78") || check_ext(filename_in, "txt")){
 		errno = EINVAL;
 		return -1;
 	}
+	*/
 	printf("comp1\n");
 	
-	b_enc = bitio_open(filename_enc, WRITE);
-	b_dec = bitio_open(filename_dec, READ);
-	if(b_enc == NULL || b_dec == NULL) return -1;
+	b_out = bitio_open(filename_out, WRITE);
+	b_in = bitio_open(filename_in, READ);
+	if(b_out == NULL || b_in == NULL) return -1;
 	
+	//( ---create the header of b_out
+	aux_32 = MAGIC;
+	memcpy(&aux_64, &aux_32, sizeof(uint32_t));
+	bitio_write(b_out, sizeof(uint32_t)*8, aux_64);
+	
+	memcpy(&aux_64, &dictionary_size, sizeof(uint32_t));
+	bitio_write(b_out, sizeof(uint32_t)*8, aux_64);
+	
+	aux_32 = 8/*bits (character)*/ + 8/* XXX bits (parent_index)*/;
+	memcpy(&aux_64, &aux_32, sizeof(uint64_t));
+	bitio_write(b_out, sizeof(uint32_t)*8, aux_64);
+	
+	memcpy(&aux_64, filename_in, strlen(filename_in));
+	bitio_write(b_out, sizeof(uint32_t)*8, aux_64);
+	//---)
+	
+	/* declaration of the hash table (tree abstractoion)*/
 	hash_table_t *t;
-	int size_of_table = 50; // ??
-	t = create_hash_table(size_of_table);
-	code_t code;
+	t = create_hash_table(dictionary_size); //??
 	
-	int res = bitio_read(b_dec, sizeof(char)*8, &result);
-	printf("comp: read %d bits (char=%c)\n",res, (char) result);
-	while(res == sizeof(char)*8){
+	LOG(INFO,"comp: read %d bits (char=%c)",res, aux_char);
+	
+	while(sizeof(char)*8 == (res = bitio_read(b_in, sizeof(char)*8, &aux_64))){
 	/* untill characters are available*/
-		node = lookup_code(t, (char) result, parent_id);
+		aux_char = (char) aux_64;
+		LOG(INFO,"read %d bits (char=%c)\",res, aux_char);
+		node = lookup_code(t, aux_char, parent_id);
 		if(node == NULL){ /* node not found */
 		
-			add_code(t, (char) result, parent_id, next_id++);
-			code.character = (char) result;
-			code.parent_id = parent_id;
-			printf("emit code #%d: <\"%c\", %d>\n", next_id-1, (char) result, parent_id);
+			add_code(t, aux_char, parent_id, next_id++);
+			LOG(DEBUG,"emit code #%d: <\"%c\", %d>", next_id-1, aux_char, parent_id);
 			
-			bzero(&result, sizeof(uint64_t));
-			memcpy(&result, &code, sizeof(code_t));
-			bitio_write(b_enc, sizeof(code_t)*8, result); /* emit code */
+			bzero(&aux_64, sizeof(uint64_t));
+			memcpy(&aux_64, &aux_c, sizeof(char));
+			bitio_write(b_out, sizeof(char)*8, aux_64); /* emit character */
+			bitio_write(b_out, 8/*XXX*/, parent_id); /* emit parent_id */
 			
 			parent_id = 0;
+			
+			/* dictionary is full: clean */
+			if(next_id == dictionary_size){ 
+				next_id = 0;
+				free_table(t);
+				t = create_hash_table(dictionary_size);
+			}
 		}
 		else{
-			printf("comp: code found\n");
+			LOG(INFO,"code found \"%c\"", aux_char);
 			parent_id = node->child_id;
 		}
-		bzero(&result, sizeof(uint64_t));
-		res = bitio_read(b_dec, sizeof(char)*8, &result);
-		printf("comp: read %d bits (char=%c)\n",res, (char) result);
 	}
 	/* just to be sure it will emits all the characters? */
 	if(node != NULL){
-		code.character = node->character;
-		code.parent_id = node->parent_id;
-		printf("codee: <\"%c\", %d>\n", node->character, node->parent_id);
-		bzero(&result, sizeof(uint64_t));
-		memcpy(&result, &code, sizeof(code_t));
-		bitio_write(b_enc, sizeof(code_t)*8, result); /* emit code */
+		LOG(INFO,"<\"%c\", %d>", node->character, node->parent_id);
+		bzero(&aux_64, sizeof(uint64_t));
+		memcpy(&aux_64, &aux_c, sizeof(char));
+		bitio_write(b_out, sizeof(char)*8, aux_64); /* emit character */
+		bitio_write(b_out, 8/*XXX*/, parent_id); /* emit parent_id */
 	}
 	
 	free_table(t);
 	
-	bitio_close(b_enc);
-	bitio_close(b_dec);
+	bitio_close(b_out);
+	bitio_close(b_in);
 	return 0;
 }
