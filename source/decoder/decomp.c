04660b51dc3146ee631e20c65bf286d14b45198a
Francesco <fpculcasi@gmail.com>
2016-07-20 17:58:33 +0200

    decomp clean of the dictionary
    The size of the dictionary is smaller than the number of code, than we need the modulus of the index of the array nodes
    However we don't have to clean the array, as such every node will replace the of the previouse (modulus of the index) and each code need only code residing on up-to-date nodes

--- source/decoder/decomp.c
+++ source/decoder/decomp.c
@@ -33,6 +33,7 @@ void decode(code_t *array, code_t node, struct bitio* b, uint8_t symbol_size){
 int decomp(const char *filename_in, const char *filename_out){
 	struct bitio *b_in, *b_out;
 	char aux_char;
+	uint size, num_of_codes;
 	uint64_t aux_64;
 	uint32_t aux_32;
 	uint32_t dictionary_size;
@@ -58,29 +59,29 @@ int decomp(const char *filename_in, const char *filename_out){
 	
 	/* get the number of codes */
 	fseek(b_in->f, 0L, SEEK_END);
-	uint size = ftell(b_in->f);
+	num_of_codes = ftell(b_in->f);
 	fseek(b_in->f, 0L, SEEK_SET);
-	size -= 3*32;                    /* neglete header size */
-	size /= symbol_size+id_size;     /* normalize the number of array elements
-	                                  * by the code dimension*/
+	num_of_codes -= 3*32;                    /* neglete header size */
+	num_of_codes /= symbol_size+id_size;     /* normalize the number of array
+													      * elements by the code dimension*/
 	/* the dictionary is clean every dictionary_size codes */
-	if(size >= dictionary_size)
-		size = dictionary_size;
+	size = (num_of_codes <= dictionary_size)? num_of_codes : dictionary_size;
 	LOG(INFO,"size: %d\n",size);
 	
 	code_t nodes[size];
 	
-	for(int i=0; i<size; i++){
+	for(int i=0; i<num_of_codes; i++){
 		if(bitio_read(b_in, id_size, &aux_64) == id_size &&
 			bitio_read(b_in, symbol_size, &aux_char) == symbol_size){
-			nodes[i].character = (char) aux_char;
-			nodes[i].parent_id = aux_64;
-			LOG(INFO, "<\"%c\", %d>\n", nodes[i].character, nodes[i].parent_id);
-			decode(nodes, nodes[i], b_out, symbol_size);
+			nodes[i%size].character = (char) aux_char;
+			nodes[i%size].parent_id = aux_64;
+			LOG(INFO, "<\"%c\", %d>\n", nodes[i%size].character, nodes[i%size].parent_id);
+			decode(nodes, nodes[i%size], b_out, symbol_size);
 		}
 		else{
-			return -1;
+			return -1
 		}
+		/*clean of the dictionary not needed*/
 	}
 	
 	bitio_close(b_in);
